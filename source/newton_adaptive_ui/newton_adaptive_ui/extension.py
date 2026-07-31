d242ce172b06efed0fc2f989d1c0257148807f63
mardigiorgio <marcodigiorgioyt@gmail.com>
2026-07-31 14:37:42 -0500

    Add Isaac Lab license headers to newton_adaptive_ui; check in bench results

--- source/newton_adaptive_ui/newton_adaptive_ui/extension.py
+++ source/newton_adaptive_ui/newton_adaptive_ui/extension.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 # Copyright (c) 2026, Newton-Adaptive workstream. SPDX-License-Identifier: BSD-3-Clause
 """Auto-loading GUI toggle for the Newton adaptive integrator.
 
